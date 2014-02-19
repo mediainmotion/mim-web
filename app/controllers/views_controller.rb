011272b8488013d438313621d5c443e546978666
Brian Kenny <brian@bkenny.com>
2014-02-19 09:02:51 +0000

    Inital commit

---
+++ app/controllers/views_controller.rb
@@ -0,0 +1,4 @@
+class ViewsController < ApplicationController
+	def homepage
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MimWeb::Application.config.secret_key_base = 'b3b88b5cbdc78685963400bab894ae2f4ab3d8f33ea52e6cad26d0d02c09d83aebf9540a25aa6aef621de444e2761bc2c0f5181fcba93172055045d88d8e0932'
